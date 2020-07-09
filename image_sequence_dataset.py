d8e7151f48d2de396248b006e3b1d5af1457550f
Jokoe66 <531211903@qq.com>
2020-07-09 17:59:06 +0100

    Added dataset api

---
+++ image_sequence_dataset.py
@@ -0,0 +1,53 @@
+import json
+
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import Compose, Normalize, Resize, ToTensor
+
+class ImageSequenceDataset(Dataset):
+
+    img_root = 'data/amap_traffic_train/'
+    ann_file = 'data/amap_traffic_annotations_%s.json'
+
+    def __init__(self, split='train', seq_max_len=5, transform=None):
+        self.ann_file = self.ann_file % split
+        self._load_anns()
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = Compose([
+                Resize(size=(720, 1280)),
+                ToTensor(),
+                Normalize(mean=[123.675, 116.28, 103.53],
+                          std=[58.395, 57.12, 57.375]),
+            ])
+        self.seq_max_len = seq_max_len
+        
+    def _load_anns(self):
+        with open(self.ann_file) as f:
+            self.anns = json.load(f)['annotations']
+    
+    def __getitem__(self, idx):
+        ann = self.anns[idx]
+        ann['imgs'] = []
+        for i in range(self.seq_max_len):
+            if i < len(ann['frames']):
+                frame = ann['frames'][i]
+                if frame['frame_name'] == ann['key_frame']:
+                    ann['key'] = i
+                img = Image.open(os.path.join(self.img_root, 
+                    ann['id'], frame['frame_name']))
+                if self.transform:
+                    img = self.transform(img)
+            else:
+                img = torch.zeros(3, 720, 1280)
+            ann['imgs'].append(img)
+        ann['imgs'] = torch.stack(ann['imgs'], -1)
+
+        return dict(imgs=ann['imgs'],
+                    key=ann['key'],
+                    len_seq=len(ann['frames']),
+                    label=ann['status'])
+
+    def __len__(self):
+        return len(self.anns)

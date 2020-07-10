8542ba2b64b87a2185fadc4e0465e10da423af63
Jokoe66 <531211903@qq.com>
2020-07-10 08:41:20 +0100

    Supported LSTM in classifier.

--- lib/classifier.py
+++ lib/classifier.py
@@ -1,3 +1,4 @@
+import os
 import tqdm
 import torch
 import mmcv
@@ -16,18 +17,25 @@ class Classifier(torch.nn.Module):
         net.eval()
         self.feat = net
         self.avg = torch.nn.AdaptiveAvgPool2d((1,1))
-        self.fc = torch.nn.Linear(2048, num_classes)
+        
+        hidden_size = kwargs.get('lstm')
+        self.lstm = torch.nn.GRU(2048, hidden_size) if hidden_size else None
+        hidden_size = hidden_size or 2048
+        self.fc = torch.nn.Linear(hidden_size, num_classes)
 
-    
-    def forward(self, input):
+    def forward(self, input, seq_len=5):
         with torch.no_grad():
             feat = self.feat(input)
         
         feat = self.avg(feat).view(len(input), -1)
+        if self.lstm:
+            feat = feat.view(seq_len, len(feat)//seq_len, -1)
+            feat, h = self.lstm(feat)
+            feat = feat.mean(0)
+        
         logit = self.fc(feat)
         return logit 
-        
-        
+
     def fit(self, dataloader, **kwargs):
         optimizer = torch.optim.SGD(
             list(self.fc.parameters()), lr=kwargs.get('lr', 1e-3), 
@@ -35,11 +43,15 @@ class Classifier(torch.nn.Module):
         lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
             optimizer, kwargs.get('milestones', [3, ]), kwargs.get('gamma', 0.1))
         criteria = torch.nn.CrossEntropyLoss()
-        
+
         max_epoch = kwargs.get('max_epoch', 4)
         best_score = 0
+        save_dir = kwargs.get('save_dir', 'checkpoints')
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
         for epoch in range(max_epoch):
             cur_lr = optimizer.param_groups[0]['lr']
+            self.train()
             for i, data in enumerate(dataloader):
                 imgs = data['imgs']
                 imgs = torch.gather(data['imgs'], -1, 
@@ -48,19 +60,20 @@ class Classifier(torch.nn.Module):
                 labels = data['label']
                 imgs = imgs.to(self.fc.weight.device)
                 labels = labels.to(self.fc.weight.device)
-                
+
                 preds = self(imgs)
                 loss = criteria(preds, labels)
                 acc = (preds.argmax(1) == labels).sum().item() / len(labels)
                 if i % kwargs.get('log_iters', 5) == 0:
                     print(f'Epoch {epoch+1}/{max_epoch} Iter: {i+1}/{len(dataloader)} '
                           f'lr: {cur_lr} loss: {loss.item()} acc: {acc}')
-                
+
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-                
+
             if kwargs.get('val_dataloader'):
+                self.eval()
                 ns = [0] * 3
                 tps = [0] * 3
                 fps = [0] * 3
@@ -78,7 +91,7 @@ class Classifier(torch.nn.Module):
 
                     with torch.no_grad():
                         preds = self(imgs)
-                    
+
                     for cls in range(len(tps)):
                         ns[cls] += (labels == cls).sum().item()
                         tps[cls] += ((labels == cls) 
@@ -92,13 +105,10 @@ class Classifier(torch.nn.Module):
                     f1s[cls] = 2 * ps[cls] * rs[cls] / (ps[cls] + rs[cls] + eps)
                 class_weights = kwargs.get('class_weights', [0.2, 0.2, 0.6])
                 score = sum(class_weights[cls] * f1s[cls] for cls in range(len(tps)))
-                if score > best_score:
-                    best_score = score
-                    torch.save(self.state_dict(),
-                               os.path.join(kwargs.get('save_dir', 'checkpoints'),
-                                            'classifier.pth'))
                 print(f'Epoch {epoch+1}/{max_epoch} lr: {cur_lr} '
-                      f'Ps: {ps} Rs: {rs} '
-                      f'f1_score: {f1s} score: {score}')
+                      f'P: {ps} R: {rs} '
+                      f'F1_score: {f1s} score: {score}')
+
+                torch.save(self.state_dict(), os.path.join(
+                    save_dir, f'classifier_epoch{epoch+1}.pth'))
             lr_scheduler.step()
-                                     

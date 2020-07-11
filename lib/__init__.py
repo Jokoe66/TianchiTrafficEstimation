3c34591355aa65fc8be3249679fbcab736c9b114
Jokoe66 <531211903@qq.com>
2020-07-11 16:41:04 +0100

    Refactored code.

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,8 +2,9 @@ import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from .image_sequence_dataset import ImageSequenceDataset
 from . import classification
 from . import lanedet
+
+from .image_sequence_dataset import ImageSequenceDataset
 
 __all__ = ['ImageSequenceDataset', ]

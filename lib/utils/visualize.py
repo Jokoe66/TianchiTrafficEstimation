a98465474d60fc752fb6254e0fde49ae167d5f23
Jokoe66 <531211903@qq.com>
2020-07-13 17:58:47 +0100

    Added main-lane vehicle distance visualization.

--- lib/utils/visualize.py
+++ lib/utils/visualize.py
@@ -33,3 +33,30 @@ def show_lanes(img, lanes, main_lane=None):
                     w / 2 - 5, h]
     draw.polygon(arrow_polygon, fill='white')
     return img
+
+
+def show_vehicle_distance(img, box_result, lanes, main_lane):
+    if isinstance(img, str):
+        img = Image.open(img)
+    elif isinstance(img, np.ndarray):
+        img = Image.fromarray(img)
+    draw = ImageDraw.Draw(img)
+
+    vehicles = np.vstack(box_result)
+    vehicles = vehicles[vehicles[:, -1] >= 0.5]
+
+    bottom_centers = vehicles[:, 2:4]
+    bottom_centers[:, 0] -= 0.5 * (vehicles[:, 2] - vehicles[:, 0])
+
+    inside_main_lane = np.hstack(
+        [point_in_polygon(bc, lanes[main_lane]) for bc in bottom_centers])
+
+    inside_bottom_centers = bottom_centers[inside_main_lane]
+
+    closest_inside_bottom_centers = inside_bottom_centers[
+        inside_bottom_centers[:, 1] == inside_bottom_centers[:, 1].max()]
+
+    for ibc in closest_inside_bottom_centers:
+        draw.line([(ibc[0], ibc[1]), (ibc[0], h)],
+                  fill=(255, 255, 255), width=2)
+    return img

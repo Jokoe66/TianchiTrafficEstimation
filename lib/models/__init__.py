3f0d811cd5bd93245f462fea7e93a25445be90c2
Jokoe66 <531211903@qq.com>
2020-10-04 16:08:33 +0100

    Add efficientnet.

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -3,6 +3,7 @@ from .hrnet import *
 from .bbn_loss import *
 from .necks import *
 from .cls_head import *
+from .efficientnet.efficientnet import *
 
 __all__ = ['Classifier', 'HRNetm', 'BBNLoss', 'PFFSeqNeck', 'PFFBPSeqNeck',
-    'DPClsHead', 'BBDPClsHead', 'BBLSTMDPClsHead']
+    'DPClsHead', 'BBDPClsHead', 'BBLSTMDPClsHead', 'EfficientNet']
---
+++ lib/models/efficientnet/__init__.py
@@ -0,0 +1,5 @@
+from .conv2d_adaptive_padding import Conv2dAdaptivePadding
+from .swish import Swish
+from .efficientnet import EfficientNet
+
+__all__ = ['Conv2dAdaptivePadding', 'Swish', 'EfficientNet']

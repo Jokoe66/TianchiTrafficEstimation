5a6a9f83b0fd9547c5bef2f5df016bd22305dea6
Jokoe66 <531211903@qq.com>
2020-10-07 14:45:03 +0100

    Update e2e_demo.py.

--- e2e_demo.py
+++ e2e_demo.py
@@ -7,7 +7,9 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import mmcv
+from mmcv.utils import Config
 from torch.utils.data import DataLoader
+from mmcls.models.builder import build_classifier
 
 from lib.datasets import ImageSequenceDataset
 from lib.models import Classifier
@@ -22,7 +24,8 @@ class Ensemble(torch.nn.Module):
         preds = []
         for model in self.models:
             pred = model(*args, **kwargs)
-            pred = F.softmax(pred, 1)
+            if (pred.sum(1) != 1).any():
+                pred = F.softmax(pred, 1)
             preds.append(pred)
         preds = torch.stack(preds, 0)
         preds = preds.mean(0)
@@ -32,6 +35,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--img_root', type=str, default='')
     parser.add_argument('--ann_file', type=str, default='')
+    parser.add_argument('--config_file', type=str, default='')
     parser.add_argument('--test_file', type=str,
                         default='/tcdata/amap_traffic_final_test_0906.json')
     parser.add_argument('--model_path', type=str, default='')
@@ -41,42 +45,25 @@ if __name__ == '__main__':
     parser.add_argument('--device', type=str, default='cuda:0')
     args = parser.parse_args()
 
+    cfg = Config.fromfile(args.config_file)
     test_set = ImageSequenceDataset(
         args.img_root,
         args.ann_file,
         'test',
         key_frame_only=args.key_frame_only,
-        input_size=(640, 360)) # consistent with training time's input_size
+        transform=cfg.test_pipeline) # consistent with training time's input_size
     test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
 
-    lstm = None if args.key_frame_only else 128
-    backbone=dict(
-        type='ResNet',
-        depth=101,
-        num_stages=4,
-        out_indices=(3,),
-        frozen_stages=4,
-        style='pytorch')
-    params= dict(
-        backbone=backbone,
-        pretrained=None,
-        bb_style='mmcls', # build backbone with mmcls or mmdet
-        bb_feat_dim=2048,
-        num_classes=4,
-        lstm=lstm,
-        bilinear_pooling=False,
-        feat_mask_dim=2,
-        feat_vec_dim=10,
-        )
     if args.ensemble:
-        self = Ensemble([Classifier(**params) for _ in range(args.ensemble)])
+        self = Ensemble(
+            [build_classifier(cfg.model) for _ in range(args.ensemble)])
         self = self.to(args.device)
         for idx, model in enumerate(self.models):
             model.load_state_dict(
                 torch.load(glob.glob(args.model_path)[idx],
                    map_location='cpu'))
     else:
-        self = Classifier(**params).to(args.device)
+        self = build_classifier(cfg.model).to(args.device)
         self.load_state_dict(torch.load(glob.glob(args.model_path)[0],
             map_location='cpu'))
     self.eval()
@@ -84,16 +71,15 @@ if __name__ == '__main__':
     for ind, data in enumerate(tqdm.tqdm(test_loader)):
         imgs = data['imgs']
         if len(imgs.shape) > 4: # frames
-            seq_len = imgs.shape[-1]
+            seq_len_max = imgs.shape[-1]
             imgs = (imgs.permute(4, 0, 1, 2, 3).contiguous()
                 .view(-1, *imgs.shape[1:4])) # t, c, h, w
         else:
-            seq_len = 1
+            seq_len_max = 1
+        data['seq_len_max'] = seq_len_max
         imgs = imgs.to(next(self.parameters()).device)
-
-        data.pop('seq_len')
         with torch.no_grad():
-            preds = self(imgs, seq_len, **data)
+            preds = self(imgs, **data)
         pred = preds.argmax(1).detach().cpu().numpy()[0]
         id = test_set.anns[ind]['id']
         results[id] = pred

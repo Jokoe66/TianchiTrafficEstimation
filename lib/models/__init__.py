a5f954ccb4c30549810616c5f490a1adb34d034b
Jokoe66 <531211903@qq.com>
2020-10-10 12:08:22 +0100

    Abandon efficientNet for mmcv compatibility and update run.sh

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -3,10 +3,10 @@ from .hrnet import *
 from .losses import *
 from .necks import *
 from .cls_head import *
-from .efficientnet.efficientnet import *
+#from .efficientnet.efficientnet import *
 from .utils import *
 
-__all__ = ['Classifier', 'MixupClassifier', 'HRNetm', 'EfficientNet',
+__all__ = ['Classifier', 'MixupClassifier', 'HRNetm', #'EfficientNet',
     'PFFSeqNeck', 'PFFBPSeqNeck', 'SequentialNecks',
     'DPClsHead', 'ClsORHead', 'LSTMClsORHead', 'KeyFrameClsORHead',
     'MultiClsHead',
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ python cal_features.py --img_root /tcdata/amap_traffic_final_test_data \
 python e2e_demo.py --img_root /tcdata/amap_traffic_final_test_data \
     --ann_file ../user_data/enriched_annotations_test_final.pkl \
     --test_file /tcdata/amap_traffic_final_test_0906.json \
-    --model_path ../user_data/res50/fold\*_best.pth \
+    --model_path ../user_data/res50/\*.pth \
     --device cuda:0 --ensemble 5 \
     --config_file configs/classifiers/classifier_r50_cls_or_aug.py
 
